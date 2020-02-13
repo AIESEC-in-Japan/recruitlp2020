5c6b27762e067c9199423b3738b04b18e9060a43
naoki_mitsuboshi <naoki.mitsuboshi@aiesec.jp>
2020-02-13 14:59:47 +0900

    Add: VIsual.vue VIsual.vueを作成し、index.vueで表示できるようにしました。

---
+++ components/Visual.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="visual-container">
+    <h1>
+      世界を変えるのは、
+      <br />誰だっていい。
+    </h1>
+
+    <h2>
+      ステートメント
+      <br />ステートメント
+      <br />ステートメント
+    </h2>
+
+    <h2 class="subtitle">
+      別に誰が変えたっていいんだよ
+      <br />キミかもしれないんだよ
+    </h2>
+
+    <h2>うんぬんかんぬんぬん。</h2>
+  </div>
+</template>
+
+<style scoped lang = "scss">
+.visual-container {
+  width: 100%;
+  height: 100vh;
+  background-image: url('../assets/images/visual-background.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: space-between;
+  justify-content: center;
+  text-align: center;
+
+  h1 {
+    color: #fff;
+    font-style: italic;
+    font-size: 30px;
+    padding: 30px 0;
+  }
+
+  h2 {
+    color: #fff;
+    font-size: 18px;
+    padding: 30px 0;
+
+    .subtitle {
+      font-style: italic;
+    }
+  }
+}
+</style>
