6c92f404f3bc7dd2742e3d3af1ae1930566c715b
customtea <kon.taku8@gmail.com>
2020-02-21 17:01:57 +0900

    Revert "Button components"
    This reverts commit 7a2b5dca7fb9995ebbb016a0b4f5c7ed1aaf8500.

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,21 @@
 <template>
   <div class="wrapper">
-  <a class="button">
     <MainVisual />
     <Visual />
-    <Button />
-  </a>
   </div>
 </template>
+
 <script>
 import MainVisual from '~/components/MainVisual.vue'
 import Visual from '~/components/Visual.vue'
-import Button from '~/components/Button.vue'
+
 
 export default {
   components: {
     MainVisual,
-    Visual,
-    Button,
+    Visual
   }
 }
 </script>
 
-<style>
-</style>
+<style></style>
