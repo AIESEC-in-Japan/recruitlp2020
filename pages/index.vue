450c85ca575ab861807aef0c6ba850d8de38bf4e
yuyatakahashi <yuya.takahashi@aiesec.jp>
2020-02-21 14:15:26 +0900

    initial commit button

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,23 @@
   <div class="wrapper">
     <MainVisual />
     <Visual />
+     <a class="Button">
+    <Button/>
+  </a>
   </div>
 </template>
 
 <script>
 import MainVisual from '~/components/MainVisual.vue'
 import Visual from '~/components/Visual.vue'
+import Button from '~/Button/Visual.vue'
 
 
 export default {
   components: {
     MainVisual,
-    Visual
+    Visual,
+    Button
   }
 }
 </script>
