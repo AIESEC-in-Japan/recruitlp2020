7a2b5dca7fb9995ebbb016a0b4f5c7ed1aaf8500
yuyatakahashi <yuya.takahashi@aiesec.jp>
2020-02-21 12:32:08 +0900

    Button components

---
+++ components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+ <a href="http://www.aiesec.jp/" class="Button">新歓説明会にいってみる</a>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+.Button{
+    display:block;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    width: 350px;
+    font-size: 14px;
+    background-color: white;
+    color:#00aeef;
+     -webkit-transition: all 0.3s ease;
+    -moz-transition: all 0.3s ease;
+    -o-transition: all 0.3s ease;
+    transition: all  0.3s ease;
+    border: solid 2px #00aeef;
+    border-radius:15px;
+    text-align: center;
+    text-decoration: none;
+
+  &:hover{
+    background-color: #00aeef;
+    color: white;       
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="wrapper">
+  <a class="button">
     <MainVisual />
     <Visual />
+    <Button />
+  </a>
   </div>
 </template>
-
 <script>
 import MainVisual from '~/components/MainVisual.vue'
 import Visual from '~/components/Visual.vue'
-
+import Button from '~/components/Button.vue'
 
 export default {
   components: {
     MainVisual,
-    Visual
+    Visual,
+    Button,
   }
 }
 </script>
 
-<style></style>
+<style>
+</style>
